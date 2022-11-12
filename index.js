4e2e266f93931f0dade905e7cd944c4daa925f64
sanyaswb <sanyaswb@gmail.com>
2022-11-12 20:51:22 +0200

    completed  24_fibonacci-loop

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fibonacci(n) {
-	// write your code here
+	let a = 1;
+	let b = 0;
+
+	for (let i = 1; i < n; i++) {
+		[a, b] = [b, a + b];
+	}
+
+	return b;
 }
 
 module.exports = fibonacci;
